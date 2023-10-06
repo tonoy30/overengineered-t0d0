8c0471560639545187be5cb4120fdee629d1eee6
Tonoy Akanda <tonoy@monetizenow.io>
2023-10-06 09:49:47 +0600

    init

---
+++ src/main/java/me/tonoy/recapspringboot/RecapSpringBootApplication.java
@@ -0,0 +1,12 @@
+package me.tonoy.recapspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecapSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/tonoy/recapspringboot/RecapSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tonoy.recapspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
